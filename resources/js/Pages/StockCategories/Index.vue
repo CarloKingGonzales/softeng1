548663ee707538d2dc5fbcdf8f77170512f606d0
Ceekay <carloking.gonzales@lorma.edu>
2021-03-27 23:57:28 +0800

    Initial commit

---
+++ resources/js/Pages/StockCategories/Index.vue
@@ -0,0 +1,64 @@
+<template>
+    <breeze-authenticated-layout>
+        <template #header>
+            <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+                Dashboard
+            </h2>
+        </template>
+
+        <div class="py-12">
+            <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+                <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                    <div class="p-6 bg-white border-b border-gray-200">
+                        <table class="min-w-full">
+                        <thead>
+                            <tr>
+                                <th class="px-6 py-3 border-b-2 border-gray-300 text-left leading-4 text-blue-500 tracking-wider">ID</th>
+                                <th class="px-6 py-3 border-b-2 border-gray-300 text-left text-sm leading-4 text-blue-500 tracking-wider">Description</th>
+                                <th class="px-6 py-3 border-b-2 border-gray-300 text-left text-sm leading-4 text-blue-500 tracking-wider">Type</th>
+                                <th class="px-6 py-3 border-b-2 border-gray-300 text-left text-sm leading-4 text-blue-500 tracking-wider">Stock account</th>
+                                <th class="px-6 py-3 border-b-2 border-gray-300 text-left text-sm leading-4 text-blue-500 tracking-wider">Created at</th>
+                                <th class="px-6 py-3 border-b-2 border-gray-300 text-left text-sm leading-4 text-blue-500 tracking-wider">Updated At</th>
+                                <th class="px-6 py-3 border-b-2 border-gray-300"></th>
+                            </tr>
+                        </thead>
+                        <tbody class="bg-white">
+                                <tr v-for="item in stock_categories">
+                                    <td class="px-6 py-4 whitespace-no-wrap border-b border-gray-500">
+                                        <div class="flex items-center">
+                                            <div>
+                                                <div class="text-sm leading-5 text-gray-800">{{item.id}}</div>
+                                            </div>
+                                        </div>
+                                    </td>
+                                    <td class="px-6 py-4 whitespace-no-wrap border-b border-gray-500">
+                                        <div class="text-sm leading-5 text-blue-900">{{ item.description }}</div>
+                                    </td>
+                                    <td class="px-6 py-4 whitespace-no-wrap border-b text-blue-900 border-gray-500 text-sm leading-5">{{ item.type }}</td>
+                                    <td class="px-6 py-4 whitespace-no-wrap border-b text-blue-900 border-gray-500 text-sm leading-5">+{{ item.stock_account }}</td>
+                                    <td class="px-6 py-4 whitespace-no-wrap border-b text-blue-900 border-gray-500 text-sm leading-5"> {{item.created_at}}</td>
+                                    <td class="px-6 py-4 whitespace-no-wrap border-b border-gray-500 text-blue-900 text-sm leading-5">{{ item.updated_at }}</td>
+                                    
+                          </tr>
+                               
+                        </tbody>
+                    </table>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </breeze-authenticated-layout>
+</template>
+
+<script>
+import Layout from "@/Layouts/Layout.vue";
+
+export default {
+    props:{
+        stock_categories: Array,
+
+
+    },
+    layout: Layout,
+};
+</script>
